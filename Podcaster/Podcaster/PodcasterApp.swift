1362d97ce573678d13076865dc532ca47f54f095
Fellipe Calleia <fellipe@calleia.com.br>
2025-09-25 21:21:14 -0300

    Delete Item.swift

--- Podcaster/Podcaster/PodcasterApp.swift
+++ Podcaster/Podcaster/PodcasterApp.swift
@@ -12,7 +12,6 @@ import SwiftData
 struct PodcasterApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
             Podcast.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
